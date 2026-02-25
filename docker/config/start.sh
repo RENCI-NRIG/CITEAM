c8d2580e5a4f954e2c1c4a41436346300f2ad4db
Karan Vahi <vahi@isi.edu>
2026-02-25 13:36:45 -0800

    A docker image that has jupyter notebook installed to test out our notebooks. this is based on the pegasus tutorial image

---
+++ docker/config/start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# set up the jupyter notebook
+if [ "x$TUTORIAL_PASSWORD" = "x" ]; then
+    TUTORIAL_PASSWORD="citeams"
+fi
+if [ "x$TUTORIAL_BASE_URL" = "x" ]; then
+    TUTORIAL_BASE_URL="/"
+fi
+ENCPASSWORD=$(python3 -c "from jupyter_server.auth import passwd;print(passwd(\"$TUTORIAL_PASSWORD\"))")
+mkdir -p /home/citeams/.jupyter
+cat >/home/citeams/.jupyter/jupyter_notebook_config.json <<EOF
+{ "ServerApp":
+   {
+        "base_url": "$TUTORIAL_BASE_URL"
+   },
+   "IdentityProvider":
+   {
+        "hashed_password": "$ENCPASSWORD"
+   }
+}
+EOF
+chown -R citeams: /home/citeams/.jupyter
+cat /home/citeams/.jupyter/jupyter_notebook_config.json
+
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
